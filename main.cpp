c0367549496e705dec437061c7177eec89f2c670
Franco N. Merenda <merenda.franco83@gmail.com>
2024-07-06 19:49:34 +0200

    Nvidia Cuda gpu to gpu test added

--- main.cpp
+++ main.cpp
@@ -3,44 +3,34 @@
 #include "report_lib/experiment_runner.h"
 
 #include "report_lib/statistic.h"
-#include "sycl_mpi/cpu_to_gpu.cpp"
-#include "cuda_mpi/cpu_to_gpu_cuda_mpi.cpp"
+// #include "sycl_mpi/cpu_to_gpu.cpp"
+// #include "cuda_mpi/cpu_to_gpu_cuda_mpi.cpp"
 #include "cuda_mpi/gpu_to_gpu_cuda_nvlink.cpp"
-#include "cuda_mpi/gpu_to_gpu_cuda_mpi.cpp"
 
 using namespace std;
 
 
 double getLatency(TimeReport timeReport){
-    return timeReport.latency.get_time();
+    return timeReport.latency.get_time_s();
 }
 
 int main(int argc, char* argv[]) {
-    unsigned int numberOfWarmups = 5; 
+    unsigned int numberOfWarmups = 0; 
     unsigned int numberOfRuns = 5;
-    unsigned int bufferSize = 1048576;
+    unsigned int numberOfElems = 1048576; //4Gb double
 
     
-    ExperimentArgs experimentArgs = ExperimentArgs(
-        argc, argv, numberOfWarmups, numberOfRuns, bufferSize
+    ExperimentArgs experimentArgs = ExperimentArgs<double>(
+        argc, argv, numberOfWarmups, numberOfRuns, numberOfElems
     );
     
-    CPUtoGPU_Sycl_MPI sycl_CPUtoGPU_experiment = CPUtoGPU_Sycl_MPI();
-
-    ExperimentRunner cpu_to_gpu_sycl_mpi = ExperimentRunner(&experimentArgs, &sycl_CPUtoGPU_experiment);
-
-    CPUtoGPU_CUDA_MPI cuda_CPUtoGPU_experiment= CPUtoGPU_CUDA_MPI();
-    ExperimentRunner cpu_to_gpu_cuda_mpi= ExperimentRunner(&experimentArgs, &cuda_CPUtoGPU_experiment);
-
-    GPUtoGPU_CUDA_NVLINK cuda_gpu_to_gpu_nvlink= GPUtoGPU_CUDA_NVLINK();
-    ExperimentRunner gpu_to_gpu_cuda_nvlink= ExperimentRunner(&experimentArgs,&cuda_gpu_to_gpu_nvlink);
+    GPUtoGPU_CUDA_NVLINK cuda_gpu_to_gpu_nvlink = GPUtoGPU_CUDA_NVLINK();
+    
+    ExperimentRunner gpu_to_gpu_cuda_nvlink= ExperimentRunner<double>(&experimentArgs, &cuda_gpu_to_gpu_nvlink);
 
 
     MPI_Init(&argc, &argv);
-
-    cpu_to_gpu_sycl_mpi.runExperiment();
     gpu_to_gpu_cuda_nvlink.runExperiment();
-    //cpu_to_gpu_cuda_mpi.runExperiment();
 
     MPI_Finalize();
 }
--- scheduler.sh
+++ scheduler.sh
@@ -7,15 +7,15 @@
 #SBATCH --output=test_output%j_out
 #SBATCH --time 00:00:20 #Be careful with this !!                          
 #SBATCH --nodes=1                                
-#SBATCH --ntasks-per-node=2 #Number of cpu cores by each node  - we can check on leonardo maximum number - 4 cores/ 1 for gpu                
+#SBATCH --ntasks-per-node=1 #Number of cpu cores by each node  - we can check on leonardo maximum number - 4 cores/ 1 for gpu                
 #SBATCH --gres=gpu:2
 # #SBATCH --mem-per-cpu=0 # This allocates memory for a dedicated way kind of
 #SBATCH --profile=All
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=FILL_WITH_EMAIL # Notification via email
+#SBATCH --mail-user=fmerenda2@studenti.unisa.it # Notification via email
 
 module load openmpi/4.1.6--gcc--12.2.0
 module load cuda/12.1
 
 echo "--------------------------- Testing MPI - Cineca Test ----"
-srun --cpu-freq=high -N 2 --ntasks-per-node=2 ./hello_world_mpi
+srun --cpu-freq=high -N 1 --ntasks-per-node=1 ./cuda_device_to_device_timing
