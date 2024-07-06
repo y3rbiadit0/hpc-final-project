db19f6bd6e38088673ec1404db5edcd2d7dd92d9
Franco N. Merenda <merenda.franco83@gmail.com>
2024-07-06 19:49:57 +0200

    Fixed gpu to gpu cuda example using template

--- cuda_mpi/gpu_to_gpu_cuda_nvlink.cpp
+++ cuda_mpi/gpu_to_gpu_cuda_nvlink.cpp
@@ -1,32 +1,34 @@
-#include <stdio.h>
-
-#include <stdlib.h>
-
 #include <mpi.h>
-
 #include <cuda_runtime.h>
+#include <iostream>
+using namespace std;
 
 #include "../report_lib/experiment.h"
 #include "../report_lib/time_report.h"
 
-class GPUtoGPU_CUDA_NVLINK: public Experiment{
+class GPUtoGPU_CUDA_NVLINK: public Experiment<double>{
 
-TimeReport run(ExperimentArgs experimentArgs) {
-	TimeReport timeReport= TimeReport();
-	// GPUs
+TimeReport run(ExperimentArgs<double> experimentArgs) {
+    cudaStream_t stream;
+    cudaEvent_t start;
+    cudaEvent_t stop;
+    TimeReport timeReport = TimeReport();
+    // Memory Copy Size 
+    float size =  experimentArgs.getBufferSize();
+
+    // Log memory usage / Amount to transfer
+    cout << "Number of elems: "<< experimentArgs.numberOfElems << " - Memory Buffer Size (Mb): " << size / 1024 << endl; 
+
+       // GPUs
     int gpuid_0 = 0;
     int gpuid_1 = 1;
  
-    // Memory Copy Size
-    float size =  experimentArgs.bufferSize * sizeof(float); // 2^26 = 67MB
- 
-    // Allocate Memory for GPU 0
-    float* dev_0;
+    // Allocate Memory
+    uint32_t* dev_0;
     cudaSetDevice(gpuid_0);
     cudaMalloc((void**)&dev_0, size);
  
-	// Allocate Memory for GPU 1
-    float* dev_1;
+    uint32_t* dev_1;
     cudaSetDevice(gpuid_1);
     cudaMalloc((void**)&dev_1, size);
  
@@ -46,45 +48,30 @@ TimeReport run(ExperimentArgs experimentArgs) {
         cudaDeviceEnablePeerAccess(gpuid_0, 0);
     }
  
-
     // Init Timing Data
-    uint32_t repeat = 10;
-    cudaEvent_t start;
-    cudaEvent_t stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
  
     // Init Stream
-    cudaStream_t stream;
     cudaStreamCreateWithFlags(&stream, cudaStreamNonBlocking);
  
-
-	//Time Report Start
-	timeReport.latency.start();
     // ~~ Start Test ~~
     cudaEventRecord(start, stream);
  
-    //Do a P2P memcpy
-    for (int i = 0; i < repeat; ++i) {
-        cudaMemcpyAsync(dev_0, dev_1, size, cudaMemcpyDeviceToDevice, stream);
-    }
- 
+    //Do a P2P memcp
+    cudaMemcpyAsync(dev_0, dev_1, size, cudaMemcpyDeviceToDevice, stream);
+    
     cudaEventRecord(stop, stream);
-	//Time Report End
-	timeReport.latency.end();
     cudaStreamSynchronize(stream);
     // ~~ End of Test ~~
  
     // Check Timing & Performance
     float time_ms;
     cudaEventElapsedTime(&time_ms, start, stop);
-    double time_s = time_ms / 1e3;
+    timeReport.latency.time_ms = time_ms;
  
-    double gb = size * repeat / (double)1e9;
-    double bandwidth = gb / time_s;
- 
-    printf("Seconds: %f\n", time_s);
-    printf("Unidirectional Bandwidth: %f (GB/s)\n", bandwidth);
+    printf("Seconds: %f\n", timeReport.latency.get_time_s());
+    printf("Unidirectional Bandwidth: %f (GB/s)\n", timeReport.bandwidth_gb(size, timeReport.latency.time_ms));
  
     if (can_access_peer_0_1 && can_access_peer_1_0) {
         // Shutdown P2P Settings
