5e8ed1a182c881169a2b8a57d80221ca805ac859
Franco N. Merenda <merenda.franco83@gmail.com>
2024-07-08 16:35:55 +0200

    Fix scaling

--- report_lib/time_report.cpp
+++ report_lib/time_report.cpp
@@ -6,7 +6,7 @@ double TimeReport::bandwidth_gb(double data_size_bytes, double time_ms){
     double data_size_gb = 0.0;
     
     if (time_s > 0.0) {
-        data_size_gb = data_size_bytes / (double) 1048576.0;
+        data_size_gb = data_size_bytes / (double) 1e9;
         bandwidth = data_size_gb / time_s;
     }
     return bandwidth;
