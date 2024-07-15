2e115a5494e5a09f6afcbf1ca6d77cfe9630cbc9
Franco N. Merenda <merenda.franco83@gmail.com>
2024-07-16 00:50:09 +0200

    Fixed environement and splitted cmake files

--- config_environment.sh
+++ config_environment.sh
@@ -1,4 +1,5 @@
-module load openmpi/4.1.6--gcc--12.2.0
-module load cuda/12.1
+module unload openmpi
 module load intel-oneapi-compilers/2023.2.1
-source /opt/intel/oneapi/setvars.sh
+source /leonardo/home/userexternal/fmerenda/intel/oneapi/setvars.sh --force
+module load openmpi/4.1.6--nvhpc--23.11
+module load cuda/12.1
