cb731880ba7954f702941eba2cdc2f35fa66d0ec
Franco N. Merenda <merenda.franco83@gmail.com>
2024-07-26 03:35:30 +0200

    Fixed single node

--- config_environment.sh
+++ config_environment.sh
@@ -1,5 +1,5 @@
 module unload openmpi
-module load intel-oneapi-compilers/2023.2.1
+#module load intel-oneapi-compilers/2023.2.1
 source /leonardo/home/userexternal/fmerenda/intel/oneapi/setvars.sh --force
 module load openmpi/4.1.6--nvhpc--23.11
-module load cuda/12.1
+module load cuda/12.3
