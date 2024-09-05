f2f233bf2502437169875f2b52b00b0d39716757
Franco N. Merenda <merenda.franco83@gmail.com>
2024-09-05 19:04:27 +0200

    Removed unused file and updated config_environment.sh

--- config_environment.sh
+++ config_environment.sh
@@ -1,5 +1,4 @@
 module unload openmpi
-#module load intel-oneapi-compilers/2023.2.1
 module load openmpi/4.1.6--nvhpc--23.11
 source /leonardo/home/userexternal/fmerenda/intel/oneapi/setvars.sh --force
 module load cuda/12.3
