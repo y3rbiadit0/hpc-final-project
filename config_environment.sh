07c36d6e199b48386737fa870d8a3a41f1f12916
Franco N. Merenda <merenda.franco83@gmail.com>
2024-09-06 02:12:33 +0200

    Fix CMakeLists.txt

--- config_environment.sh
+++ config_environment.sh
@@ -1,4 +1,4 @@
 module unload openmpi
-module load openmpi/4.1.6--nvhpc--23.11
 source /leonardo/home/userexternal/fmerenda/intel/oneapi/setvars.sh --force
+module load openmpi/4.1.6--nvhpc--23.11
 module load cuda/12.3
